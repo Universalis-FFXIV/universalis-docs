662618287f0d68d4230a13cb9f6c2867564fd01f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-22 19:46:44 -0700

    Add API version selector

--- components/RestDocumentation/RestDocumentation.tsx
+++ components/RestDocumentation/RestDocumentation.tsx
@@ -1,17 +1,22 @@
-import { Title, Text, Container, Space } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { Title, Text, Container, Space, Input, Group, SimpleGrid } from '@mantine/core';
+import { ChevronDownIcon } from '@modulz/radix-icons';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { SwaggerSchema } from '../../data/swagger/types';
 import { RestEndpoint } from '../RestEndpoint/RestEndpoint';
 
-const schemaUrl = 'https://localhost:5001/docs/swagger/v1/swagger.json';
+function getSchemaUrl(version: string) {
+  return `https://localhost:5001/docs/swagger/${version}/swagger.json`;
+}
 
 export function RestDocumentation() {
+  const [schemaVersion, setSchemaVersion] = useState<string>('v1');
+
   const [schema, setSchema] = useState<SwaggerSchema>();
   useEffect(() => {
-    fetch(schemaUrl)
+    fetch(getSchemaUrl(schemaVersion))
       .then((res) => res.json())
       .then(setSchema);
-  }, []);
+  }, [schemaVersion]);
 
   if (schema == null) {
     return <div />;
@@ -19,7 +24,21 @@ export function RestDocumentation() {
 
   return (
     <Container>
-      <Title id="rest-api">{schema.info.title} REST API</Title>
+      <SimpleGrid cols={2}>
+        <Title id="rest-api">{schema.info.title} REST API</Title>
+        <Group position="right">
+          <Text size="lg">Version</Text>
+          <Input
+            component="select"
+            rightSection={<ChevronDownIcon />}
+            sx={{ width: 200 }}
+            onChange={(e: ChangeEvent<HTMLSelectElement>) => setSchemaVersion(e.target.value)}
+          >
+            <option value="v1">v1</option>
+            <option value="v2">v2</option>
+          </Input>
+        </Group>
+      </SimpleGrid>
       <Text dangerouslySetInnerHTML={{ __html: schema.info.description }} />
       <Space h="xl" />
       {Object.keys(schema.paths).flatMap((path) =>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,55 @@
+import {
+  AppShell,
+  Navbar,
+  Text,
+  Title,
+  Divider,
+  UnstyledButton,
+  Group,
+  Header,
+} from '@mantine/core';
+import Link from 'next/link';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { RestDocumentation } from '../components/RestDocumentation/RestDocumentation';
+import useStyles from './index.styles';
 
 export default function HomePage() {
+  const { classes } = useStyles();
+
   return (
-    <>
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} p="xs">
+          <Navbar.Section>
+            <Title className={classes.title}>Universalis</Title>
+          </Navbar.Section>
+          <Divider my="sm" />
+          <Navbar.Section grow mt="md">
+            <UnstyledButton>
+              <Group>
+                <Text>
+                  <Link href="/">REST API</Link>
+                </Text>
+              </Group>
+            </UnstyledButton>
+          </Navbar.Section>
+        </Navbar>
+      }
+      header={
+        <Header height={60} p="xs">
+          <div />
+          <ColorSchemeToggle />
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      })}
+    >
       <RestDocumentation />
-    </>
+    </AppShell>
   );
 }
