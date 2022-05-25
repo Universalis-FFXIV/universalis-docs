0daa8c9a4d5d0d710aadec690f4d5825c48ed2dd
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-25 13:23:22 -0700

    Extract table of contents to separate component

--- components/RestDocumentation/RestDocumentation.tsx
+++ components/RestDocumentation/RestDocumentation.tsx
@@ -1,21 +1,13 @@
-import { Title, Text, Container, Space, Input, Group, SimpleGrid, List, Box } from '@mantine/core';
+import { Title, Text, Container, Space, Input, Group, SimpleGrid } from '@mantine/core';
 import { ChevronDownIcon } from '@modulz/radix-icons';
 import { ChangeEvent, useEffect, useState } from 'react';
-import Link from 'next/link';
 import { SwaggerSchema } from '../../data/swagger/types';
 import { RestComponent } from '../RestComponent/RestComponent';
 import { RestWelcome } from '../RestWelcome/RestWelcome';
 import { RestEndpoint } from '../RestEndpoint/RestEndpoint';
 import useStyles from './RestDocumentation.styles';
 import { LoadingDocumentation } from '../LoadingDocumentation/LoadingDocumentation';
-
-function nameToId(name: string): string {
-  return `schema-${name.toLowerCase()}`;
-}
-
-function tagToId(tag: string) {
-  return tag.toLowerCase().replace(/\s+/g, '-');
-}
+import { RestTableOfContents } from '../RestTableOfContents/RestTableOfContents';
 
 export function RestDocumentation() {
   const { classes } = useStyles();
@@ -42,45 +34,6 @@ export function RestDocumentation() {
     }))
   );
 
-  const endpointHeaders = simpleSchema.map(({ endpoint }) => endpoint.tags[0]);
-  const endpointIds = endpointHeaders.map(tagToId);
-
-  const components = Object.keys(schema.components.schemas).map((name) => ({
-    name,
-    component: schema.components.schemas[name],
-  }));
-
-  const componentHeaders = components.map(({ name }) => name);
-  const componentIds = componentHeaders.map(nameToId);
-
-  const toc = (
-    <Box
-      sx={(theme) => ({
-        width: 400,
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-        padding: 8,
-      })}
-    >
-      <Text size="xl">Table of contents</Text>
-      <Text size="lg">Endpoints</Text>
-      <List type="ordered">
-        {endpointHeaders.map((h, i) => (
-          <List.Item key={`toc${h}`}>
-            <Link href={`#${endpointIds[i]}`}>{h}</Link>
-          </List.Item>
-        ))}
-      </List>
-      <Text size="lg">Entities</Text>
-      <List type="ordered">
-        {componentHeaders.map((h, i) => (
-          <List.Item key={`toc${h}`}>
-            <Link href={`#${componentIds[i]}`}>{h}</Link>
-          </List.Item>
-        ))}
-      </List>
-    </Box>
-  );
-
   return (
     <Container>
       <SimpleGrid cols={2}>
@@ -100,7 +53,7 @@ export function RestDocumentation() {
       </SimpleGrid>
       <RestWelcome />
       <Space h="md" />
-      {toc}
+      <RestTableOfContents schema={schema} />
       <Space h="xl" />
       <Title id="rest-api-endpoints" className={classes.sectionHeader} mb={16}>
         Endpoints
