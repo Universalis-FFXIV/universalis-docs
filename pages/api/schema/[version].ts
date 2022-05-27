4fd4fde55439316c97c82abcddf115bd94846309
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-26 21:47:17 -0700

    Add primitive logging to the API

--- pages/api/schema/[version].ts
+++ pages/api/schema/[version].ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { stderr, stdout } from 'process';
 import { getSchemaUrl } from '../../../data/api/universalis';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -11,9 +12,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     // The response type depends on if the data is compressed or not,
     // so we just read it as a JSON object and re-serialize it either way.
-    const data = await fetch(getSchemaUrl(version)).then((r) => r.json());
+    const url = getSchemaUrl(version);
+    stdout.write(`${new Date().toUTCString()}\tFetching schema from ${url}\n`);
+    const data = await fetch(url).then((r) => r.json());
     res.status(200).json(data);
   } catch (err) {
+    stderr.write(`${new Date().toUTCString()}\tFailed to get schema: ${err}\n`);
     res.status(500).json({ error: 'Failed to get API schema.' });
   }
 }
