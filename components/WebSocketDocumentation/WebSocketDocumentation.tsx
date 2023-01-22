0dd2cf59a62916594b1b30aff6f647d7c1a3720c
Alexander Blair <ablair@jagtech.io>
2023-01-22 10:51:20 -0800

    AB-WS-Add: Add websocket data format
    Provide initial documentation for the WS BSON data,
    this is formatted in JSON for parsability

--- components/WebSocketDocumentation/WebSocketDocumentation.tsx
+++ components/WebSocketDocumentation/WebSocketDocumentation.tsx
@@ -128,6 +128,81 @@ ws.send(serialize({ event: "subscribe", channel: "listings/add{world=63}" }));`}
         game regions, so you probably won&apos;t want to listen on any channel without any filters
         at all.
       </Text>
+        <Title id="bson-formatting" mt={32}>
+            BSON Data Format
+        </Title>
+        <Text mt={16}>
+            All data sent over the websocket is formatted via BSON, same as the subscribe requests,
+            and will need to be passed through a BSON decoder/parser in order to be used in your
+            application, please make sure to review the comments as some fields can be NULL or
+            not included in some requests. Converting this for your specific language will require
+            review of your languages BSON parser, as there may be case sensitivity issues.  The
+            following is a JSON representation for your convenience.
+        </Text>
+        <Prism.Tabs mt={16}>
+            <Prism.Tab label="Node" language="json">
+                {`{
+  /* This corresponds to the subscribe information */
+  "event": "listings/add",
+  /* Item ID being sent in this message */
+  "item": 23122,
+  /* World ID being sent in this message */
+  "world": 2,
+  /* This key is not present for sales subscriptions */
+  "listings": [
+    {
+      /* This field is often NULL */
+      "creatorID": 0,
+      /* This is often an empty string */
+      "creatorName": "",
+      "hq": true,
+      "isCrafted": false,
+      /* Most recent upload date */
+      "lastReviewTime": 1674368621,
+      /* Unique hash used to identify a listing for removal */
+      "listingID": "b5399af38b63eea4a3ceca6aa68692451076109b3a20c39f4be79c522f541cfc",
+      "materia": [
+        {
+          /* Slot ID the materia is in */
+          "slotID": 0,
+          /* This is the materia id, which is not the item ID, use https://xivapi.com/materia/<id> to look up details if needed */
+          "materiaID": 0
+        }
+      ],
+      "onMannequin": false,
+      "pricePerUnit": 20999,
+      "quantity": 10,
+      "retainerCity": 12,
+      "retainerID": "98702c8262940d99d76cdc236324c18351a8d6dcf764d1f97b376f87bc22036f",
+      "retainerName": "xyzzy",
+      "sellerID": "74ce52ff2a2b7625bacdd4dace29d3abbbf2431e71f2aa8519905f437dd9fed9",
+      "stainID": 0,
+      "total": 209990,
+      /* This field is often NULL */
+      "worldID": 0,
+      /* This field is often NULL */
+      "worldName": ""
+    }
+  ],
+  /* This key is not present for listings subscriptions */
+  "sales": [
+    {
+      "buyerName": "xyzzy",
+      "hq": false,
+      "onMannequin": false,
+      "pricePerUnit": 20999,
+      "quantity": 10,
+      "timestamp": 1674411943,
+      "total": 209990,
+      /* This field is often NULL */
+      "worldID": 0,
+      /* This field is often NULL */
+      "worldName": ""
+    }
+  ]
+}`}
+            </Prism.Tab>
+        </Prism.Tabs>
       <Space mt={200} />
     </Container>
   );
